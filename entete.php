0615547e2efa5c57e9ea4b27432110c06149fa98
Isaac willy <wgwada971@gmail.com>
2015-10-25 15:14:16 +0100

    entete de la page d'accueil

---
+++ entete.php
@@ -0,0 +1,3 @@
+<div id="website-logo">
+  <a href="http://www.femball.fr/"><img class="website-logo-image" width="180" src="http://www.femball.fr/s/misc/logo.gif?t=1444048292" alt="Femball - Championnat de D1 Féminine 2015-2016"/></a>
+</div>
